cea24753fadd0fe160378fc6645413583c87a23e
chandan <chandanmahato624@gmail.com>
2025-01-09 20:02:39 +0000

    Till home placeholder

--- lib/weatherscreens.dart
+++ lib/weatherscreens.dart
@@ -2,11 +2,36 @@ import 'package:flutter/material.dart';
 
 // ignore: camel_case_types
 class weatherscreen extends StatelessWidget {
-  const weatherscreen ({super.key})
+  const weatherscreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
-    
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Weather App",
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: (){}, 
+            icon: const Icon(Icons.refresh),),
+        ],
+      ),
+      body: Column(
+        children: [
+          // main card
+          Card(
+            child: Column(
+              children: [
+                Text("300 F"),
+              ],
+            ),
+          ),
+          const sizedBox
+        ],
+      )
+    );
   }
 }
